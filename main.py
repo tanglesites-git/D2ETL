8ea5db8ecb3d0f2c578e7239850ba0f78dea99d6
Joseph <tanglesites@hotmail.com>
2024-10-24 12:47:33 -0500

    Add setup

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-from requests from get
+from requests import get
+
+
+def download_manifest():
+    pass
+
+
+def write_manifest():
+    pass
+
+
+def download_all_json_files():
+    pass
+
+
+def download_world_content():
+    pass
+
+
+def write_world_content():
+    pass
+
+
+def main():
+    pass
+
 
 if __name__ == '__main__':
     pass
